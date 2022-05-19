c8caab92c26edce4d44b7c5fedda8b91b9d507a0
akbarhlubis <alubis87@gmail.com>
2022-05-19 10:41:27 +0700

    memberikan nav baru

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
 var typed = new Typed("#typed", {
-    strings: ["Welcome to my personal Website"],
-    typeSpeed: 110,
-    startDelay: 500,
-    loop: true,
-    loopCount: Infinity,
-    backDelay: 1000
-  });
-  
+  strings: ["Welcome to my personal Website"],
+  typeSpeed: 110,
+  startDelay: 500,
+  loop: true,
+  loopCount: Infinity,
+  backDelay: 1000,
+});
+
+let popup = document.querySelector(".popup"),
+  a = document.querySelector(".btn-link");
+
+a.addEventListener("click", openPop);
+
+function openPop() {
+  popup.style.display = "Block";
+}
+
+window.addEventListener("click", closePop);
+
+function closePop(e) {
+  if (e.target == popup) {
+    popup.style.display = "none";
+  }
+}
